11cb2ffef25e4f00bc4c5e4593cbd3a33563d6f1
Nipuni Obe <obe@gmail.com>
2021-02-01 14:00:13 -0500

    Initial Commit

---
+++ 2 Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Button Challenge
+//
+//  Created by Nipuni Obe on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
