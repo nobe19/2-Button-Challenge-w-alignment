1a6ce493358b8208c4e6cdfa044bc99ec1546bfe
Nipuni Obe <obe@gmail.com>
2021-02-03 12:14:47 -0500

    Completed 2 button press w/ alignment (copy of original 2 button challenge) with textColor and textAlignment

--- 2 Button Challenge/ViewController.swift
+++ 2 Button Challenge/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("✌️viewDidLoad has run!")
     }
 
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        print("🗣 The message button was pressed!")
+        messageLabel.text = "Howdy!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = NSTextAlignment.left
+    }
+    
+    
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        print("🤪 The other message button was pressed!")
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemOrange
+        messageLabel.textAlignment = NSTextAlignment.right
+    }
+    
 }
 
